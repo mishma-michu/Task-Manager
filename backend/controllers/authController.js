06db126bbfe857d90113f77f8deaa4af7d120e75
mishma-michu <mishmabenny@gmail.com>
2025-12-28 15:27:11 +0530

    Register backend integration done

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,36 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
 const generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+const register = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({ username, email, password });
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const login = async (req, res) => {
@@ -22,4 +50,4 @@ const login = async (req, res) => {
   });
 };
 
-module.exports = { login };
+module.exports = { login, register };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const { body } = require('express-validator');
 const { login } = require('../controllers/authController');
+const { register } = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post(
+  '/register',
+  [
+    body('username', 'Username is required').notEmpty(),
+    body('email', 'Valid email is required').isEmail(),
+    body('password', 'Password min 6 chars').isLength({ min: 6 }),
+  ],
+  register
+);
 
 router.post(
   '/login',
